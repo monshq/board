02509d8ba8a48d7091da52c596abad31d1e19cbe
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-15 14:33:40 +0400

    Add nested layout for Dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
+# https://gist.github.com/907555
+
 module ApplicationHelper
+  def inside_layout(layout = 'application', &block)
+    render :inline => capture_haml(&block), :layout => "layouts/#{layout}"
+  end
 end

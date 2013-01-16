c8f7b2d6ad5f79b2a9eb957ca848aeacf0b2e996
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-16 15:04:44 +0400

    Fix users factory to create valid confirmed users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 FactoryGirl.define do
   factory :user, aliases: [:seller, :sender, :recipient] do
-    email    Faker::Internet.email
-    password Faker::Lorem.sentence
+    email Faker::Internet.email
+
+    p = Faker::Lorem.sentence
+    password p
+    password_confirmation p
+
+    confirmed_at Time.now
   end
 end

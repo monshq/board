33d1c41f1ce1a77f1ca918d997068c78b3b5c886
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-18 16:11:54 +0400

    Flatten layouts and move shared parts into partials

--- app/helpers/application_helper.rb
+++
@@ -1,7 +0,0 @@
-# https://gist.github.com/907555
-
-module ApplicationHelper
-  def inside_layout(layout = 'application', &block)
-    render :inline => capture_haml(&block), :layout => "layouts/#{layout}"
-  end
-end

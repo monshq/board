95ff81ee2d33bce0f1280a7bd72fca224ffb8fa7
ILoGM <a.a.mikka@gmail.com>
2013-01-24 09:46:49 +0400

    Update application controller for localization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale
 
   def after_sign_in_path_for(user)
     dashboard_items_path
+  end
+
+  def set_locale
+    if params[:locale]
+      I18n.locale = params[:locale]
+    else
+      I18n.locale = http_accept_language.preferred_language_from(I18n.available_locales)
+      redirect_to url_for(locale: I18n.locale, only_path: true)
+    end
+  end
+
+  def default_url_options(options={})
+    { locale: I18n.locale, only_path: true }
   end
 end

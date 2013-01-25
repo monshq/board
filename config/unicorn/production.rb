0eb920ebd0aa20c94aa628be747fbc7f8e43a734
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-25 18:39:20 +0400

    Add error log path to Unicorn config

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,1 +1,4 @@
-pid "#{File.dirname(File.dirname(File.dirname(__FILE__)))}/tmp/pids/unicorn.pid"
+app_root = File.dirname(File.dirname(File.dirname(__FILE__)))
+
+pid         "#{app_root}/tmp/pids/unicorn.pid"
+stderr_path "#{app_root}/log/unicorn.stderr.log"

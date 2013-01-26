d3ffe1ed671ab0e58bc9633c5bf022368e55c134
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-27 00:55:18 +0400

    Satisfy capistrano-unicorn gem but actually use all defaults

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,4 +1,1 @@
-app_root = File.dirname(File.dirname(File.dirname(__FILE__)))
-
-pid         "#{app_root}/tmp/pids/unicorn.pid"
-stderr_path "#{app_root}/log/unicorn.stderr.log"
+# capistrano-unicorn gem requires this file

c190f8d8b0f95a70b000581283dae56c4ebf30de
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-28 12:18:57 +0400

    Clean up deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,14 +6,12 @@ before 'deploy:setup', 'rvm:install_ruby'
 
 require 'rvm/capistrano'
 
-# default is '--deployment --quiet'
-# --quiet is bad, hides useful error messages
 set :bundle_flags, '--deployment'
+
 require 'bundler/capistrano'
 
 set :application, 'board'
 set :repository,  'https://github.com/vassilevsky/board.git'
-#set :branch,      'master'
 set :branch,      'feature-17-deployment'
 
 server '192.168.20.9', :web, :app, :db, primary: true
@@ -24,9 +22,6 @@ default_run_options[:pty] = true
 
 set :deploy_via, :remote_cache
 
-# after 'deploy:update_code' do
-# if executed this way ^^^, executes after all other callbacks
-# but it needs to be executed before asset compilation
 before 'deploy:assets:precompile' do
   put """production:
   adapter: postgresql

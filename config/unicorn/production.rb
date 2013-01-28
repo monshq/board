9f859cb4acf0c5081ca18a25cfebde69eed82dea
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-28 11:26:43 +0400

    Shut down old Unicorn instance from a new one

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,1 +1,18 @@
 pid 'tmp/pids/unicorn.pid'
+
+after_fork do |master, worker|
+  old_pid_path = "#{master.pid}.oldbin"
+
+  if File.exists? old_pid_path
+    old_master_pid = File.read(old_pid_path).to_i
+    worker_number = worker.nr + 1 # first is 0
+
+    if worker_number < master.worker_processes
+      signal = :WINCH # gracefully shut down workers
+    else
+      signal = :QUIT
+    end
+
+    Process.kill signal, old_master_pid
+  end
+end

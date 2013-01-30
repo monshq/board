a1df40dccb50ed76dd6671c5fad18d7a97d0f0ad
Alexey Mikka <a.a.mikka@gmail.com>
2013-01-30 14:28:43 +0400

    Update user authorizer

--- app/authorizers/users_authorizer.rb
+++ app/authorizers/users_authorizer.rb
@@ -1,11 +1,10 @@
 class UsersAuthorizer < ApplicationAuthorizer
   def self.readable_by?(user)
-    user.has_role? :admin
+    (user.has_role?(:admin) || user.has_role?(:user))
   end
 
   def readable_by?(user)
-    # user.has_role?(:admin) || user.has_role?(:user)
-    true
+    (user.has_role?(:admin) || user.has_role?(:user))
   end
 
   def updateable_by?(user)

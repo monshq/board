a484537e6f6c15f87b1e0dee160bcee9cfc228f9
Alexey Mikka <a.a.mikka@gmail.com>
2013-01-31 16:26:04 +0400

    Update athority config to add new ability action 'become' for authorizers

--- config/initializers/authority.rb
+++ config/initializers/authority.rb
@@ -37,12 +37,13 @@ Authority.configure do |config|
   #
   # Defaults are as follows:
   #
-  # config.abilities =  {
-  #   :create => 'creatable',
-  #   :read   => 'readable',
-  #   :update => 'updatable',
-  #   :delete => 'deletable'
-  # }
+  config.abilities =  {
+    :create => 'creatable',
+    :read   => 'readable',
+    :update => 'updatable',
+    :delete => 'deletable',
+    :become => 'becomable'
+  }
 
   # SECURITY_VIOLATION_HANDLER
   # ==========================

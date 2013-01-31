fbab4c623c39337a32d101af4731d4aabb7ca890
Alexey Mikka <a.a.mikka@gmail.com>
2013-01-31 16:19:06 +0400

    Renamed controller and add new action

--- app/controllers/admin/admin_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::AdminController < Admin::ApplicationController
-  before_filter :authenticate_user!
-
-  def become_user
-    return unless current_user.has_role?(:admin)
-    sign_in User.find(params[:id]), bypass: true
-    redirect_to root_url # or user_root_url
-  end
-end
---
+++ app/controllers/admin/admins_controller.rb
@@ -0,0 +1,11 @@
+class Admin::AdminsController < Admin::ApplicationController
+  before_filter :authenticate_user!
+
+  def create
+    @user = User.find(params[:user_id])
+    # authorize!(:login_as, @user.role)
+    session[:source_user] ||= current_user.id
+    sign_in(@user, bypass: true)
+    redirect_to after_sign_in_path_for(@user)
+  end
+end

96d0607d3423d902a1da3aa77e6b9791e430db2c
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-01-31 15:15:55 +0400

    Link each tag to new set of selected tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,10 +6,27 @@ class TagsController < ApplicationController
   end
 
   def index
+    @selected_tags = []
+    compose_linked_ids
   end
 
   def show
     @selected_tags = Tag.find params[:id].split('+') # /tags/1+2
     @tagged_items = Item.tagged_with @selected_tags
+    compose_linked_ids
+  end
+
+  def compose_linked_ids
+    @linked_ids = {}
+
+    @all_tags.each do |tag|
+      linked_tags = if @selected_tags.include? tag
+                      @selected_tags - [tag]
+                    else
+                      @selected_tags + [tag]
+                    end
+      linked_tag_ids = linked_tags.collect { |t| t.id }
+      @linked_ids[tag.id] = linked_tag_ids.join('+')
+    end
   end
 end

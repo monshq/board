2a04a33db2198d1a96f0e5b6007a06696be38965
Alexey Mikka <a.a.mikka@gmail.com>
2013-01-31 19:12:46 +0400

    Add authorizer to action

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -3,7 +3,7 @@ class Admin::AdminsController < Admin::ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    # authorize!(:login_as, @user.role)
+    # authorize_action_for(current_user)
     session[:source_user] ||= current_user.id
     sign_in(@user, bypass: true)
     redirect_to after_sign_in_path_for(@user)

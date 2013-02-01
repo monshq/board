b3d6a37f8e242e519482c13f3934719b824e75e0
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-02-01 13:51:01 +0400

    Test search by 1 word

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+require 'spec_helper'
+
+feature 'Чтобы лучше указать что мне нужно, я хочу воспользоваться формой поиска' do
+  scenario 'Я ввожу слово в поле поиска и вижу только объявления с этим словом' do
+    FactoryGirl.create :item, description: 'Продаётся велосипед'
+    FactoryGirl.create :item, description: 'Продаётся телевизор'
+
+    visit root_path
+    fill_in I18n.t('find'), with: 'велосипед'
+    click_button I18n.t('search')
+
+    page.should     have_text 'Продаётся велосипед'
+    page.should_not have_text 'Продаётся телевизор'
+  end
+end

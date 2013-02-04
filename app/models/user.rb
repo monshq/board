585152262b522a25871ecfc9de69d3f01851b25d
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-04 15:51:51 +0400

    Add state machine to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,43 @@ class User < ActiveRecord::Base
          :validatable,
          :confirmable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :state, :state_changed_at
 
   has_many :items, dependent: :destroy, foreign_key: 'seller_id'
   has_many :received_messages, class_name: 'Message', dependent: :destroy, foreign_key: 'recipient_id'
   has_many :sent_messages,     class_name: 'Message', dependent: :destroy, foreign_key: 'sender_id'
 
   self.authorizer_name = 'UsersAuthorizer'
+
+  state_machine :initial => :active do
+    after_transition :on => :ban,   :do => :set_state_change_date_time
+    after_transition :on => :unban, :do => :set_state_change_date_time
+
+    event :ban do
+      transition :active => :banned
+    end
+
+    event :unban do
+      transition :banned => :active
+    end
+
+    state :banned do
+      def visible?
+        false
+      end
+    end
+
+    state :active do
+      def visible?
+        true
+      end
+    end
+  end
+
+  scope :active, lambda { where("state = ?", :active) }
+  scope :banned, lambda { where("state = ?", :banned) }
+
+  def set_state_change_date_time
+    @state_changed_at = Time.new
+  end
 end

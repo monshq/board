177e4a70259728e5107b4a47bea10c45526fa6f5
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-04 18:11:55 +0400

    Refactor admin controller

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -3,7 +3,6 @@ class Admin::AdminsController < Admin::ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    # authorize_action_for(current_user)
     session[:source_user] ||= current_user.id
     sign_in(@user, bypass: true)
     redirect_to after_sign_in_path_for(@user)

455fbb6a4d3e4ef31d229c7ef7b966be699dfafe
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-04 11:26:28 +0400

    Remove needless tables from this branch

--- db/schema.rb
+++ db/schema.rb
@@ -44,17 +44,6 @@ ActiveRecord::Schema.define(:version => 20130204055448) do
     t.string   "file"
   end
 
-  create_table "roles", :force => true do |t|
-    t.string   "name"
-    t.integer  "resource_id"
-    t.string   "resource_type"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-  end
-
-  add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], :name => "index_roles_on_name"
-
   create_table "tags", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -83,12 +72,5 @@ ActiveRecord::Schema.define(:version => 20130204055448) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
-
-  create_table "users_roles", :id => false, :force => true do |t|
-    t.integer "user_id"
-    t.integer "role_id"
-  end
-
-  add_index "users_roles", ["user_id", "role_id"], :name => "index_users_roles_on_user_id_and_role_id"
 
 end

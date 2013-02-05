ce1230b316c3118991db348ae4567c7898bd719e
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-05 18:37:46 +0400

    Add ban and allow methods to admin controller

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -7,4 +7,16 @@ class Admin::AdminsController < Admin::ApplicationController
     sign_in(@user, bypass: true)
     redirect_to after_sign_in_path_for(@user)
   end
+
+  def ban_user
+    @user = User.find(params[:user_id])
+    @user.ban
+    redirect_to users_path
+  end
+
+  def allow_user
+    @user = User.find(params[:user_id])
+    @user.allow
+    redirect_to users_path
+  end
 end

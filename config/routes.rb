e731b46aff55ae92239ae75c5644421e696f4aea
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-06 19:22:02 +0400

    Exercises with routes (fails)

--- config/routes.rb
+++ config/routes.rb
@@ -19,11 +19,23 @@ Board::Application.routes.draw do
 
   resources :users
 
-  scope module: :admin do
-    resource :admin, only: [:create] do
-      post 'ban/:user_id' => 'admins#ban_user', as: :ban_user
-      post 'allow/:user_id' => 'admins#allow_user', as: :allow_user
+  namespace :admin do
+  # scope module: :admin do
+    #resource :admin, only: [:create] do
+      #post 'ban/:user_id' => 'admins#ban_user', as: :ban_user
+      #post 'allow/:user_id' => 'admins#allow_user', as: :allow_user
+    #end
+    resources :photos, only: [] do
+      resources :bans, only: [:new, :create, :destroy]
     end
+
+    resources :users, only: [] do
+      resources :ban, only: [:new, :create, :destroy]
+    end
+    #resources :users do
+      #resources :ban, only: [:new, :create]
+      #resources :allow, only: [:create]
+    #end
   end
 
   resources :items, only: [:index]

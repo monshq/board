08c17d45bebfbc175c149fdd782bdd92637d8948
Denis Kirichenko <denis.kirichenko@gmail.com>
2013-02-06 12:56:12 +0400

    Add focus: true to rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -55,6 +55,9 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+
   config.before(:each) do
     DatabaseCleaner.start
   end

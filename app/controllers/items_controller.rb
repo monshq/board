f1f5c1ae29a5008bf6c6f6ea1626e7abdf743423
Ilya Vassilevsky <vassilevsky@gmail.com>
2013-02-06 14:09:32 +0400

    Prettify ItemsController#index - oldschool if/else

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   def index
-    @items = if params[:keywords].empty?
-               Item.all
-             else
-               Item.search params[:keywords]
-             end
+    unless params[:keywords].empty?
+      @items = Item.search params[:keywords]
+    else
+      @items = Item.all
+    end
   end
 end

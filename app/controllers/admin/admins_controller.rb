4323de94f1a26a41e34d1c30c93b16dcea25a801
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-07 10:01:51 +0400

    Replace before filter to Application controller

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,6 +1,4 @@
 class Admin::AdminsController < Admin::ApplicationController
-  before_filter :authenticate_user!
-
   def create
     @user = User.find(params[:user_id])
     session[:source_user] ||= current_user.id

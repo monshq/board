689a12009cef44ee1d2e8b755e9c9333ddc40ed9
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-07 16:21:21 +0400

    Add new action and create stub to ban_photos controller

--- app/controllers/admin/ban_photos_controller.rb
+++ app/controllers/admin/ban_photos_controller.rb
@@ -1,3 +1,12 @@
 class Admin::PhotosController < Admin::ApplicationController
 
+  def new
+    @photo = Photo.find(params[:photo_id])
+    @admin_comment = @photo.admin_comments.build({ action_type: 'Ban' })
+  end
+
+  def create
+
+  end
+
 end

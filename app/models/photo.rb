362605b9f1b78c4e0bccf19117a207ddf9f53dd9
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-07 16:39:11 +0400

    Add state_machine to Photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,19 +1,44 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :file, :is_main
-
   include Authority::Abilities
 
-  attr_accessible :file, :state, :state_changed_at
+  attr_accessible :file, :state, :state_changed_at, :is_main
+
   belongs_to :item
   has_many :admin_comments, as: :bannable
   mount_uploader :file, PhotoUploader
 
   state_machine :state, :initial => :active do
+    after_transition :on => :allow, :do => :set_state_change_date_time
+    after_transition :on => :ban, :do => :set_state_change_date_time
+
     event :archivate do
       transition :active => :archived
+    end
+
+    event :ban do
+      transition :active => :banned
+    end
+
+    event :allow do
+      transition :banned => :active
+    end
+
+    state :banned do
+      def visible?
+        false
+      end
+    end
+
+    state :active do
+      def visible?
+        true
+      end
     end
   end
 
   scope :active, lambda { where("state <> ?", :archived) }
 
+  def set_state_change_date_time
+    @state_changed_at = Time.new
+  end
 end

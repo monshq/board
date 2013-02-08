79962b0363c561db87ed738831219cf266010839
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-08 12:41:35 +0400

    Update admin_comment creation in BanPhotos controllerin new and create actions cause has_one association become

--- app/controllers/admin/ban_users_controller.rb
+++ app/controllers/admin/ban_users_controller.rb
@@ -2,13 +2,13 @@ class Admin::BanUsersController < Admin::ApplicationController
 
   def new
     @user = User.find(params[:user_id])
-    @admin_comment = @user.admin_comments.build({ action_type: 'Ban' })
+    @admin_comment = @user.build_admin_comment({ action_type: 'Ban' })
   end
 
   def create
     @user = User.find(params[:user_id])
 
-    @user.admin_comments.build params[:admin_comment]
+    @user.build_admin_comment params[:admin_comment]
 
     if @user.save
       flash[:notice] = "User banned" # TODO: Добавить локализацию

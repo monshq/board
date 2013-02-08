efc91612cfd3ecc96bb02e0909be1f55463e69c4
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-08 12:40:49 +0400

    Update admin_comment creation in BanPhotos controller cause has_one association become

--- app/controllers/admin/ban_photos_controller.rb
+++ app/controllers/admin/ban_photos_controller.rb
@@ -2,7 +2,7 @@ class Admin::BanPhotosController < Admin::ApplicationController
 
   def new
     @photo = Photo.find(params[:photo_id])
-    @admin_comment = @photo.admin_comments.build({ action_type: 'Ban' })
+    @admin_comment = @photo.build_admin_comment({ action_type: 'Ban' })
   end
 
   def create

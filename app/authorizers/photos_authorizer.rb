10402fe9a5253487282766cef094f4830250f9f3
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-08 12:23:05 +0400

    Update photo authorizer, add new abilities

--- app/authorizers/photos_authorizer.rb
+++ app/authorizers/photos_authorizer.rb
@@ -9,5 +9,10 @@ class PhotosAuthorizer < ApplicationAuthorizer
   end
 
   def updatable_by?(user)
+    (user.id == resource.item.seller_id)
+  end
+
+  def managable_by?(user)
+    user.has_role? :admin
   end
 end

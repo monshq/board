8b431953567b9f7ecd4ca8b14e2e2a743eca9f15
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-11 18:42:47 +0400

    Fix #photo_banned_email test

--- spec/mailers/board_mailer_spec.rb
+++ spec/mailers/board_mailer_spec.rb
@@ -31,15 +31,23 @@ describe BoardMailer do
   describe '#photo_banned_email' do
 
     it 'Отсылает письмо о том что картинка заблокирована' do
-      @user = FactoryGirl.create :user
-      BoardMailer.photo_banned_email(@user).deliver
+      @photo = FactoryGirl.create :photo
+      @comment = Faker::Lorem.sentence
+      PROTOCOL = 'http://'
+      HOST_WITH_PORT = 'localhost:3000/'
+
+      BoardMailer.photo_banned_email(PROTOCOL, HOST_WITH_PORT, @photo, @comment).deliver
 
       message = ActionMailer::Base.deliveries.last
 
-      message.to.should include @user.email
+      message.to.should include @photo.item.seller.email
       message.subject.should have_text I18n.t('board_mailer.photo_banned_email.photo_banned_title')
       message.body.should have_text I18n.t('board_mailer.photo_banned_email.photo_banned_title')
       message.body.should have_text I18n.t('board_mailer.photo_banned_email.photo_banned_message')
+      message.body.should have_text I18n.t('board_mailer.photo_banned_email.photo_banned_link')
+      message.body.should have_link @photo.file
+      message.body.should have_text I18n.t('board_mailer.photo_banned_email.photo_banned_reason', reason: @comment)
+      message.body.should have_text @comment
     end
 
   end

4c00f55c5021f80d45305c5f7e3ec28a968ac49e
ILoGM <a.a.mikka@gmail.com>
2013-02-11 23:22:58 +0400

    Add ban user spec helpers

--- spec/support/admin_helper.rb
+++ spec/support/admin_helper.rb
@@ -3,3 +3,16 @@ def become_user(user)
   visit users_path
   click_link 'Become user'
 end
+
+def new_ban_user(user)
+  visit users_path
+  click_link 'Ban user'
+end
+
+def ban_user(user, comment)
+  new_ban_user(user)
+
+  fill_in 'Comment', with: comment
+
+  click_button 'Create Admin comment'
+end

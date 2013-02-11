a130e98a5fd2af207be610623eece9e1f93a4fc9
ILoGM <a.a.mikka@gmail.com>
2013-02-12 02:21:58 +0400

    Refactor attach_photos_to_item

--- spec/support/admin_helper.rb
+++ spec/support/admin_helper.rb
@@ -17,15 +17,18 @@ def ban_user(user, comment)
   click_button 'Create Admin comment'
 end
 
-def attach_photos_to_item(item)
+def attach_photos_to_item(item, only_one = false)
   click_link I18n.t(:sign_out)
   sign_in_user item.seller
 
   click_link I18n.t(:add_photo)
 
   attach_file 'Изображение 1', Rails.root.join('spec', 'support', 'test_image_1.jpg')
-  attach_file 'Изображение 2', Rails.root.join('spec', 'support', 'test_image_2.jpg')
-  attach_file 'Изображение 3', Rails.root.join('spec', 'support', 'test_image_3.jpg')
+  unless (only_one)
+    attach_file 'Изображение 2', Rails.root.join('spec', 'support', 'test_image_2.jpg')
+    attach_file 'Изображение 3', Rails.root.join('spec', 'support', 'test_image_3.jpg')
+  end
+
   click_button 'Save changes'
 
   click_link I18n.t(:sign_out)

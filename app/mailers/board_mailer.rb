0a138cb1ecb755816b80a0c15056fc552b0cfaba
Alexey Mikka <a.a.mikka@gmail.com>
2013-02-11 15:55:44 +0400

    Refactor photo_banned_email action

--- app/mailers/board_mailer.rb
+++ app/mailers/board_mailer.rb
@@ -10,8 +10,8 @@ class BoardMailer < ActionMailer::Base
     mail(:to => user.email, :subject => I18n.t('board_mailer.user_banned_email.user_banned_title'))
   end
 
-  def photo_banned_email(user)
-    mail(:to => user.email, :subject => I18n.t('board_mailer.photo_banned_email.photo_banned_title'))
+  def photo_banned_email(protocol, host, photo, comment)
+    @params = {image_link: photo.file.url, reason: comment, protocol: protocol, host: host}
+    mail(:to => photo.item.seller.email, :subject => I18n.t('board_mailer.photo_banned_email.photo_banned_title'))
   end
-
 end

7e92eac4c4e53b3d0f442963b500bed2906079a5
ILoGM <a.a.mikka@gmail.com>
2013-02-12 01:32:03 +0400

    Add attach photos to item  helper

--- spec/support/admin_helper.rb
+++ spec/support/admin_helper.rb
@@ -16,3 +16,17 @@ def ban_user(user, comment)
 
   click_button 'Create Admin comment'
 end
+
+def attach_photos_to_item(item)
+  click_link I18n.t(:sign_out)
+  sign_in_user item.seller
+
+  click_link I18n.t(:add_photo)
+
+  attach_file 'Изображение 1', Rails.root.join('spec', 'support', 'test_image_1.jpg')
+  attach_file 'Изображение 2', Rails.root.join('spec', 'support', 'test_image_2.jpg')
+  attach_file 'Изображение 3', Rails.root.join('spec', 'support', 'test_image_3.jpg')
+  click_button 'Save changes'
+
+  click_link I18n.t(:sign_out)
+end

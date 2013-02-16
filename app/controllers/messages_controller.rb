0e5d0b364985a7a4a1e27a98c0fb053bde272be9
Denis Kirichenko <denis.kirichenko@gmail.com>
2013-02-17 00:28:47 +0400

    добавление тестов и доработка in-place авторизации для написания сообщений

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,31 @@
 class MessagesController < ApplicationController
-  #before_filter :authenticate_user!
-
   def new
     item = Item.find(params[:item_id])
-    @message = item.messages.build session.delete(:message)
-    if @message.text.present?
-      send_message(@message)
+    if params[:return] && current_user
+      @message = item.messages.build session.delete(:message)
+      send_message
+    else
+      @message = item.messages.build
     end
   end
 
   def create
     item = Item.find(params[:item_id])
     if current_user
-      send_message item.messages.build(params[:message])
+      @message = item.messages.build(params[:message])
+      send_message
     else
       session[:message] = params[:message]
-      session[:user_return_to] = new_item_message_path(item)
-      redirect_to new_user_session_path
+      session[:user_return_to] = new_item_message_path(item, :return => true)
+      authenticate_user!
     end
   end
 
-  def send_message(message)
-    message.sender = current_user
-    message.recipient = @message.item.seller
-    if message.save
+private
+  def send_message
+    @message.sender = current_user
+    @message.recipient = @message.item.seller
+    if @message.save
       flash[:notice] = t(:new_message_sent)
       redirect_to item_path(@message.item)
     else

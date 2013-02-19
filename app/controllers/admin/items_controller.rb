2480b95497e6a0a2408fcebaa6d351cb723aaea9
Denis Kirichenko <denis.kirichenko@gmail.com>
2013-02-19 16:11:36 +0400

    repair tests after rebase

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,11 +8,16 @@ class Admin::ItemsController < Admin::ApplicationController
     @item = Item.find(params[:id])
 
     if @item.update_attributes(params[:item])
-      @item.set_tags(params[:tags])
+      @item.set_tags parse_tags(params[:tags])
 
       redirect_to items_path, notice: t(:item_updated)
     else
       render action: "edit"
     end
+  end
+
+private
+  def parse_tags(tags_str)
+    tags_str.split(',').map{|t| t.strip}
   end
 end

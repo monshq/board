51cef29e7726231552f6e1930a369ac909c15bad
Dmitriy Parshin <d.i.parshin@gmail.com>
2013-03-01 09:36:35 +0400

    Review features

--- lib/routing_filter/board_locale.rb
+++ lib/routing_filter/board_locale.rb
@@ -2,7 +2,7 @@ module RoutingFilter
   class BoardLocale < Locale
     
     def self.locales_pattern
-      %r(^/([a-z]{2})(?=/|$)) #use first path with 2 lowcase letters as locale code
+      @@locales_pattern ||= %r(^/([a-z]{2})(?=/|$)) #use first path with 2 lowcase letters as locale code
     end
     
   end
